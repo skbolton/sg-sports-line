ef111b1eb4e10de7d9ea02478d8554c08e26d6c4
Stephen Bolton <stephenkbolton@gmail.com>
2020-06-20 15:31:11 -0600

    Add context api for adding athletes to events

--- apps/events/lib/events.ex
+++ apps/events/lib/events.ex
@@ -4,10 +4,27 @@ defmodule Events do
   """
 
   alias DB.Repo
-  alias Events.Event
+
+  alias Events.{
+    Event,
+    EventAthlete
+  }
+
+  alias Athletes.Athlete
 
   def create_event(params) do
     Event.new_event_changeset(%Event{}, params)
     |> Repo.insert()
+  end
+
+  def add_athlete_to_event(%{event_id: event_id, athlete_id: athlete_id} = params) do
+    athlete_task = Task.async(fn -> Repo.get(Athlete, athlete_id) end)
+    event_task = Task.async(fn -> Repo.get(Event, event_id) end)
+
+    with %Event{} <- Task.await(event_task),
+         %Athlete{} <- Task.await(athlete_task) do
+      EventAthlete.new_event_athlete_changeset(%EventAthlete{}, params)
+      |> Repo.insert()
+    end
   end
 end
--- apps/events/lib/events/event_athlete.ex
+++ apps/events/lib/events/event_athlete.ex
@@ -1,5 +1,6 @@
 defmodule Events.EventAthlete do
   use Ecto.Schema
+  import Ecto.Changeset
   alias Events.Event
   alias Athletes.Athlete
 
@@ -9,5 +10,11 @@ defmodule Events.EventAthlete do
     belongs_to :event, Event
     belongs_to :athlete, Athlete
     timestamps()
+  end
+
+  def new_event_athlete_changeset(event_athlete, params \\ %{}) do
+    event_athlete
+    |> cast(params, [:cost, :event_id, :athlete_id])
+    |> validate_required([:cost, :event_id, :athlete_id])
   end
 end
