4d78ecba7e2b2b19df2ab9496bcf806c29b313b1
Stephen Bolton <stephenkbolton@gmail.com>
2020-06-20 15:37:51 -0600

    Add sheet schema

---
+++ apps/sheets/lib/sheets/sheet.ex
@@ -0,0 +1,12 @@
+defmodule Sheets.Sheet do
+  use Ecto.Schema
+
+  schema "sheets" do
+    field :venmo_username, :string
+    field :email, :string
+    field :verified, :boolean, default: false
+    belongs_to :event, Event
+    many_to_many :athletes, EventAthlete, join_through: "sheets_athletes"
+    timestamps()
+  end
+end
