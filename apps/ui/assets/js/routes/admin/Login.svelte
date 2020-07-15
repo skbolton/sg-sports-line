005b02f3dd714020e8474c58859199db59917400
Stephen Bolton <stephenkbolton@gmail.com>
2020-07-14 20:46:14 -0600

    Move into root admin route on successful login

--- apps/ui/assets/js/routes/admin/Login.svelte
+++ apps/ui/assets/js/routes/admin/Login.svelte
@@ -1,3 +1,28 @@
+<script>
+  import { navigate } from 'svelte-routing'
+  import api from '../../api'
+  import adminStore from '../../storage.js'
+
+  let email = ""
+  let password = ""
+
+  $: submittable = email.length > 0 && password.length > 0
+
+  const login = () => {
+    if (!submittable) {
+      return
+    }
+
+    api.users.login(email, password)
+      .then(api.getData)
+      .then(({ admin = false }) => {
+        adminStore.storeAdmin(admin)
+        navigate('/admin/')
+      })
+  }
+
+</script>
+
 <style>
   form {
     width: 33%;
@@ -8,12 +33,12 @@
 
 <section class="hero is-link is-fullheight">
   <div class="hero-body aic jcc">
-    <form class="box">
+    <form class="box" on:submit|preventDefault={login}>
       <h1 class="title has-text-black">Admin Panel</h1>
       <div class="field mb-5">
         <label class="label">Email</label>
         <div class="control has-icons-left">
-          <input class="input" type="text" placeholder="Email">
+          <input autofocus class="input" type="text" bind:value={email} placeholder="Email" on:keypress={event => email = event.target.value}>
           <span class="icon is-small is-left">
             <i class="ri-user-fill"></i>
           </span>
@@ -22,7 +47,7 @@
       <div class="field mb-5">
         <label class="label">Password</label>
         <div class="control has-icons-left">
-          <input class="input" type="password" placeholder="Password">
+          <input class="input" type="password" bind:value={password} placeholder="Password" on:keypress={event => password = event.target.value}>
           <span class="icon is-small is-left">
             <i class="ri-lock-fill"></i>
           </span>
@@ -30,7 +55,7 @@
       </div>
       <div class="field mb-5">
         <div class="control">
-          <input type="submit" value="Log in" class="button is-link">
+          <input type="submit" disabled={!submittable} value="Log in" class="button is-link">
         </div>
       </div>
     </form>
