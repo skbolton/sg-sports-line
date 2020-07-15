31aa1b882e609e39fc700bbde0ff69119092da8e
Stephen Bolton <stephenkbolton@gmail.com>
2020-07-14 20:46:02 -0600

    Scaffold out basic admin events route

--- apps/ui/assets/js/routes/admin/Admin.svelte
+++ apps/ui/assets/js/routes/admin/Admin.svelte
@@ -1,8 +1,10 @@
 <script>
   import { Router, Route } from 'svelte-routing'
+  import Events from './Events.svelte'
   import Login from './Login.svelte'
 </script>
 
 <Router>
   <Route component="{Login}"></Route>
+  <Route path="/" component="{Events}" />
 </Router>
---
+++ apps/ui/assets/js/routes/admin/Events.svelte
@@ -0,0 +1,6 @@
+<script>
+  import Navbar from '@common/Nav.svelte'
+</script>
+
+<Navbar/>
+<h1 class="title">Events</h1>
