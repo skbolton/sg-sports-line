76a75ba712c9adbb04f257ae8b03a243714ac6ac
Stephen Bolton <stephenkbolton@gmail.com>
2020-07-28 18:14:45 -0600

    Scaffold out basic error handling on events creation

--- apps/ui/lib/ui_web/controllers/event_controller.ex
+++ apps/ui/lib/ui_web/controllers/event_controller.ex
@@ -4,6 +4,8 @@ defmodule UIWeb.EventController do
 
   import UIWeb.Auth
 
+  require Logger
+
   plug :ensure_admin when action in [:create]
 
   def create(conn, %{"event" => event}) do
@@ -19,6 +21,13 @@ defmodule UIWeb.EventController do
         conn
         |> put_status(:created)
         |> json(%{event: event})
+
+      {:error, reason} ->
+        Logger.error(inspect(reason))
+
+        conn
+        |> put_status(400)
+        |> json(%{error: "There was an error"})
     end
   end
 end
