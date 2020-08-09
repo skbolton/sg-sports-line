0b1ddabc86041348a95cf155ee960d30d9ab5080
Stephen Bolton <stephenkbolton@gmail.com>
2020-08-08 18:44:55 -0600

    Add Events page to admin using new common components

--- apps/ui/assets/js/routes/admin/Admin.svelte
+++ apps/ui/assets/js/routes/admin/Admin.svelte
@@ -3,12 +3,15 @@
   import Events from './Events.svelte'
   import Login from './Login.svelte'
   import CreateEvent from './CreateEvent.svelte'
-  import Event from './Event.svelte'
+  /* import Event from './Event.svelte' */
+  /* <Route path="create-event" component="{CreateEvent}"></Route> */
+    /* <Route path="/event/:id" let:params> */
+    /*   <Event id="{params.id}"/> */
+    /* </Route> */
 </script>
 
 <Router url="">
   <Route path="login" component="{Login}"></Route>
-  <Route path="create-event" component="{CreateEvent}"></Route>
     <Route path="/event/:id" let:params>
       <Event id="{params.id}"/>
     </Route>
--- apps/ui/assets/js/routes/admin/Events.svelte
+++ apps/ui/assets/js/routes/admin/Events.svelte
@@ -1,7 +1,23 @@
 <script>
   import {Link} from 'svelte-routing'
+  import api from '@api'
   import Navbar from '@common/Nav.svelte'
-  import EventList from './EventList.svelte'
+  import EventList from '@common/events/EventList.svelte'
+
+  const adminEventsQuery = `
+    query {
+      allEvents {
+        name
+        id
+        eventStart
+        eventEnd
+      }
+    }
+  `
+
+  const allEvents = api.graph(adminEventsQuery)
+    .then(({ allEvents }) => allEvents)
+
 </script>
 
 <style>
@@ -16,8 +32,12 @@
 <Navbar/>
 <div class="container">
   <h1 class="title">Events</h1>
-  <EventList/>
+  {#await allEvents}
+    Loading...
+    {:then events}
+      <EventList events={events} on:eventClicked={console.log} />
+  {/await}
 </div>
 <Link to="create-event">
-  <button class="button is-link is-size-4">+</button>
+  <button class="button is-primary is-size-4">+</button>
 </Link>
