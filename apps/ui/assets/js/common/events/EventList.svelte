adc8d4123bc6a0496d349f4f797c0cb730d154ed
Stephen Bolton <stephenkbolton@gmail.com>
2020-08-08 18:43:52 -0600

    Add EventList and EventListItem common components

---
+++ apps/ui/assets/js/common/events/EventList.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+  import EventListItem from './EventListItem.svelte'
+  export let events
+
+  const dispatch = createEventDispatcher()
+</script>
+
+<div class="columns">
+  {#each events as event}
+    <div class="column is-one-third-desktop">
+      <EventListItem on:click={() => dispatch('eventClicked', event)} event={event}/>
+    </div>
+  {/each}
+</div>
---
+++ apps/ui/assets/js/common/events/EventListItem.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let event = {}
+</script>
+
+<div class="card" on:click>
+  <section class="card-content">
+    <main class="content">
+      <h1 class="title is-4">{event.name}</h1>
+      <p>
+        <strong>Event Start</strong>
+      </p>
+      <p>{new Date(event.eventStart).toDateString()}
+
+      <p>
+        <strong>Event End</strong>
+      </p>
+      <p>{new Date(event.eventEnd).toDateString()}
+    </main>
+  </section>
+</div>
