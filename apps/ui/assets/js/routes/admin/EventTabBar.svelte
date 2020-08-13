456487aa008aa93a6b4bfd8922e13e58822e5cf7
Stephen Bolton <stephenkbolton@gmail.com>
2020-08-12 21:01:59 -0600

    Add EventTabBar

---
+++ apps/ui/assets/js/routes/admin/EventTabBar.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let page = 'athletes'
+</script>
+
+<div class="tabs is-boxed">
+  <ul>
+    <li class:is-active={page === "athletes"}>
+      <a>
+        <span class="icon is-small"><i class="ri-shield-user-line"></i></span>
+        <span>
+          Athletes
+        </span>
+      </a>
+    </li>
+    <li class:is-active={page === "sheets"}>
+      <a>
+        <span class="icon is-small"><i class="ri-file-list-line"></i></span>
+        <span>
+          Sheets
+        </span>
+      </a>
+    </li>
+  </ul>
+</div>
