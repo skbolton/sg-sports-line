167d78615eeacde56f63be089bd72fcb6c6b028a
Stephen Bolton <stephenkbolton@gmail.com>
2020-08-21 19:14:04 -0600

    Add id to event athlete grphql type

--- apps/ui/lib/ui_web/graph/types/event_athlete.ex
+++ apps/ui/lib/ui_web/graph/types/event_athlete.ex
@@ -3,6 +3,8 @@ defmodule UIWeb.Graph.Schema.Types.EventAthlete do
   use Absinthe.Schema.Notation
 
   object :event_athlete do
+    @desc "id for event athlete"
+    field :id, non_null(:id)
     @desc "How much the athlete costs in the event"
     field :cost, non_null(:integer)
     @desc "How much the athlete won in the event"
